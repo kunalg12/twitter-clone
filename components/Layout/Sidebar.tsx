a3c3168f6d3d053428c7d32c227b7a9bb9975f90
kunalg12 <kunalgursal2923@gmail.com>
2023-03-30 23:24:18 +0530

    updated

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,32 +1,34 @@
-import { BsHouseFill, BsBellFill } from 'react-icons/bs'
-import { FaUser } from 'react-icons/fa'
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SiderbarLogo from "./SidebarLogo";
 
-const  Sidebar = () => {
-    const items =[{
-        label: 'Home',
-        href: '/',
-        icons:BsHouseFill
+const Sidebar = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icons: BsHouseFill,
     },
     {
-        label:'Notifications',
-        href: '/notifications',
-        icons:BsBellFill
+      label: "Notifications",
+      href: "/notifications",
+      icons: BsBellFill,
     },
     {
-        label:'Profile',
-        href: '/users/123',
-        icons: FaUser,
-    }
-];
+      label: "Profile",
+      href: "/users/123",
+      icons: FaUser,
+    },
+  ];
   return (
-    <div className='col-span-1 h-full pr-4 md:pr-6'>
-        <div className='flex flex-col items-end'>
-            <div className='space-y-2 lg:w-[230px]'>
-                <SiderbarLogo/>
-            </div>
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SiderbarLogo />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
