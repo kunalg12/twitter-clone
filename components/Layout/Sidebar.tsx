e192cbbbd85bf73e476fe3c651f311fa1a5d4a43
kunalg12 <kunalgursal2923@gmail.com>
2023-03-30 17:58:42 +0530

    first commit

---
+++ components/Layout/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { BsHouseFill, BsBellFill } from 'react-icons/bs'
+import { FaUser } from 'react-icons/fa'
+
+const  Sidebar = () => {
+    const items =[{
+        label: 'Home',
+        href: '/',
+        icons:BsHouseFill
+    },
+    {
+        label:'Notifications',
+        href: '/notifications',
+        icons:BsBellFill
+    },
+    {
+        label:'Profile',
+        href: '/users/123',
+        icons: FaUser,
+    }
+];
+  return (
+    <div className='col-span-1 h-full pr-4 md:pr-6'>
+        <div className='flex flex-col items-end'>
+            <div className='space-y-2 lg:w-[230px]'>
+                <SiderbarLogo/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
