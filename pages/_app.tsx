da22184b10051a578c422b79d7e41279c704d8b1
kunalg12 <kunalgursal2923@gmail.com>
2023-04-04 16:27:04 +0530

    input updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
-import Modal from '../components/Modal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Modal actionLabel='Submit' isOpen title='Test Modal'/>
      <Layout> 
       <Component {...pageProps}/>
      </Layout>
