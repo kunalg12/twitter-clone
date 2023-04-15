ff84346b7f14e5a53588609621f47f7a15fb8f6b
kunalg12 <kunalgursal2923@gmail.com>
2023-04-15 15:25:43 +0530

    uppdated

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -9,8 +9,8 @@ const RegisterModal = () => {
   const registerModal = useRegisterModal();
 
   const [email, setEmail] = useState("");
-  const [name , setName] = useState("");
-  const [username , setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -30,40 +30,46 @@ const RegisterModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4 ">
-      <Input placeholder="Email" 
-      onChange={(e) => setEmail(e.target.value)}
-      value="email"
-      disabled={isLoading}
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
       />
-      <Input placeholder="Name" 
-      onChange={(e) => setName(e.target.value)}
-      value="name"
-      disabled={isLoading}
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
       />
-      <Input placeholder="Username" 
-      onChange={(e) => setUsername(e.target.value)}
-      value="username"
-      disabled={isLoading}
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
       />
-      <Input placeholder="Password" 
-      onChange={(e) => setPassword(e.target.value)}
-      value="password"
-      disabled={isLoading}
+      <Input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
       />
     </div>
   );
 
-  return <>
-    <Modal
-    disabled={isLoading}
-    isOpen={registerModal.isOpen}
-    title="Create an account"
-    actionLabel="Register"
-    onClose={registerModal.onClose}
-    onSubmit={onSumbit}
-    body={bodyContent}
-    />
-  </>;
+  return (
+    <>
+      <Modal
+        disabled={isLoading}
+        isOpen={registerModal.isOpen}
+        title="Create an account"
+        actionLabel="Register"
+        onClose={registerModal.onClose}
+        onSubmit={onSumbit}
+        body={bodyContent}
+      />
+    </>
+  );
 };
 
 export default RegisterModal;
