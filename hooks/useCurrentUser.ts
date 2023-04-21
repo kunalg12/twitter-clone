0520de4266c5e3bce6bae36eea45584ea0ee1d9b
kunalg12 <kunalgursal2923@gmail.com>
2023-04-21 20:23:29 +0530

    useCurrentUser updated

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,1 +1,16 @@
 import useSWR from "swr";
+import fetcher from "../libs/fetcher";
+import { use } from "react";
+
+const useCurrentUser = () => {
+    const { data, isLoading, error, mutate } = useSWR('/api/current', fetcher);
+
+    return{
+        data,
+        isLoading,
+        error,
+        mutate
+    }
+};
+
+export default useCurrentUser;
